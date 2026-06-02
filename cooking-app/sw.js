d8bf74929193014fe70ff82c171e2a267c48dffb
xixi70515-lab <xixi70515@gmail.com>
2026-06-02 21:17:25 +0800

    Add files via upload

---
+++ cooking-app/sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'cooking-guide-v1';
+const URLS = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(resp => {
+        if (resp.ok) {
+          const clone = resp.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return resp;
+      }).catch(() => cached)
+    )
+  );
+});
